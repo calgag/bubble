015d44241f87c9669d772e609a2cb608f5b66233
calgag <gaglianc@oregonstate.edu>
2018-08-20 20:58:07 -0700

    back + works

--- FRONT/src/app/app.component.spec.ts
+++ FRONT/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'bubble'`, async(() => {
+  it(`should have as title 'FRONT'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bubble');
+    expect(app.title).toEqual('FRONT');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bubble!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FRONT!');
   }));
 });
