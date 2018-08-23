1a04578fd1ae7a89bcddd97bbd6ddaf52f4d9986
calgag <calgagi@gmail.com>
2018-08-23 12:29:31 -0700

    some experimenting

---
+++ FRONT/src/app/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import { PaperScope } from 'paper';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App row">
+        <div className="col-md-12">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
