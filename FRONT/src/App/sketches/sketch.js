f876a9d6c37f666f8a13c7fc3a350408453f697a
calgag <calgagi@gmail.com>
2018-08-28 14:07:04 -0700

    it's working I'm so happygit add .!

--- FRONT/src/App/sketches/sketch.js
+++ FRONT/src/App/sketches/sketch.js
@@ -1,33 +1,14 @@
-
 export default function sketch (p) {
-  let rotation = 0;
-
   p.setup = function () {
-    p.createCanvas(600, 400, p.WEBGL);
-  };
-
-  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-    if (props.rotation){
-      rotation = props.rotation * Math.PI / 180;
-    }
+    p.createCanvas(640, 480);
+    p.frameRate(60);
   };
 
   p.draw = function () {
-    p.background(100);
-    p.noStroke();
-
-    p.push();
-    p.translate(-150, 100);
-    p.rotateY(rotation);
-    p.rotateX(-0.9);
-    p.box(100);
-    p.pop();
-
-    p.noFill();
-    p.stroke(255);
-    p.push();
-    p.translate(500, p.height*0.35, -200);
-    p.sphere(300);
-    p.pop();
-  };
+    if (p.mouseIsPressed) {
+      p.fill(0);
+    } else {
+      p.fill(255);
+    }
+    p.ellipse(p.mouseX, p.mouseY, 80, 80);  };
 };
