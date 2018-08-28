387de811dd1f004de679b4415ea8993464350f17
calgag <calgagi@gmail.com>
2018-08-28 13:45:03 -0700

    got p5 working!

--- FRONT/src/index.js
+++ FRONT/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Canvas from './Canvas/Canvas';
+import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Canvas />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
