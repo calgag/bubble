ad36a9a70c04b16996ec13704b9ffe9e92613765
calgag <calgagi@gmail.com>
2018-08-28 15:17:39 -0700

    need to start working on TAF now

--- FRONT/src/App/sketches/sketch.js
+++ FRONT/src/App/sketches/sketch.js
@@ -1,27 +1,43 @@
 export default function sketch (p) {
-  var x = 100;
-  var y = 100;
-  var speed = 2;
-  var r, g, b;
+  var diameter = 100;
+  var speed = [];
+  var direction = [];
+  var x = [], y = [];
+  var numberOfBubbles = 20;
 
+  // Setup before draw
+  // 1. Gets random color and sets
+  // 2. Get random positions for each bubble
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.frameRate(60);
-    r = Math.random() * 255;
-    g = Math.random() * 255;
-    b = Math.random() * 255;
-  };
-
-  p.draw = function() {
-    p.clear();
+    var r = Math.random() * 255;
+    var g = Math.random() * 255;
+    var b = Math.random() * 255;
     p.strokeWeight(2);
     p.stroke(r, g, b);
     p.fill(r, g, b, 127);
-    x += speed;
-    if(x > (p.width - 50) || x < (0 + 50)){
-      speed = 0 - speed;
+    for(var i = 0; i < numberOfBubbles; i++){
+      speed.push(1);
+      x.push(Math.random() * p.windowWidth);
+      y.push(Math.random() * p.windowHeight);
     }
-    p.ellipse(x,y,100,100);
+  };
+
+  // Draw runs in loop
+  // 1. Clear canvas
+  // 2. Draws each bubble
+  p.draw = function() {
+    p.clear();
+
+    for(var i = 0; i < numberOfBubbles; i++){
+      x[i] += speed[i];
+      if(x[i] > (p.windowWidth - (diameter/2)) || x[i] < (0 + (diameter/2))){
+        speed[i] = 0 - speed[i];
+        console.log("Direction of " + i + " bubble changed.");
+      }
+      p.ellipse(x[i], y[i], diameter, diameter);
+    }
   };
 
   p.windowResized = function () {
