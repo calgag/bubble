320d946d95b3182bb13f2f90d84b6bfc4cdb6761
calgag <calgagi@gmail.com>
2018-08-28 17:37:42 -0700

    f;ajfe

--- FRONT/src/App/sketches/sketch.js
+++ FRONT/src/App/sketches/sketch.js
@@ -1,9 +1,12 @@
 export default function sketch (p) {
   var diameter = 100;
-  var speed = [];
+  var speedx = [];
+  var speedy = [];
   var direction = [];
   var x = [], y = [];
-  var numberOfBubbles = 20;
+  var r = [], g = [], b = [];
+  var speed = 1;
+  var numberOfBubbles = 27;
 
   // Setup before draw
   // 1. Gets random color and sets
@@ -11,16 +14,25 @@ export default function sketch (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.frameRate(60);
-    var r = Math.random() * 255;
-    var g = Math.random() * 255;
-    var b = Math.random() * 255;
     p.strokeWeight(2);
-    p.stroke(r, g, b);
-    p.fill(r, g, b, 127);
+
     for(var i = 0; i < numberOfBubbles; i++){
-      speed.push(1);
-      x.push(Math.random() * p.windowWidth);
-      y.push(Math.random() * p.windowHeight);
+      r[i] = Math.random() * 255;
+      g[i] = Math.random() * 255;
+      b[i] = Math.random() * 255;
+      x[i] = (Math.random() * p.windowWidth);
+      y[i] = (Math.random() * p.windowHeight);
+      direction[i] = Math.random() * 360;
+      speedx[i] = (Math.random()*(speed + speed)) - speed;
+      speedy[i] = Math.sqrt((speed * speed) - (speedx[i] * speedx[i]));
+      if(Math.random() >= 0.50000){
+        speedy[i] = 0 - speedy[i];
+      }
+      if((x[i] < (diameter/2)) || (x[i] > (p.windowWidth-(diameter/2)))) {
+        i = i - 1;
+      }else if((y[i] < (diameter/2)) || (y[i] > (p.windowHeight-(diameter/2)))) {
+        i = i - 1;
+      }
     }
   };
 
@@ -31,9 +43,22 @@ export default function sketch (p) {
     p.clear();
 
     for(var i = 0; i < numberOfBubbles; i++){
-      x[i] += speed[i];
+      p.stroke(r[i], g[i], b[i]);
+      p.fill(r[i], g[i], b[i], 127);
+      x[i] += speedx[i];
+      y[i] += speedy[i];
+      if(y[i] > (p.windowHeight - (diameter/2)) || y[i] < (0 + (diameter/2))){
+        r[i] = Math.random() * 255;
+        g[i] = Math.random() * 255;
+        b[i] = Math.random() * 255;
+        speedy[i] = 0 - speedy[i];
+        console.log("Direction of " + i + " bubble changed.");
+      }
       if(x[i] > (p.windowWidth - (diameter/2)) || x[i] < (0 + (diameter/2))){
-        speed[i] = 0 - speed[i];
+        r[i] = Math.random() * 255;
+        g[i] = Math.random() * 255;
+        b[i] = Math.random() * 255;
+        speedx[i] = 0 - speedx[i];
         console.log("Direction of " + i + " bubble changed.");
       }
       p.ellipse(x[i], y[i], diameter, diameter);
